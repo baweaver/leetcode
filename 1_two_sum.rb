c79d4248a2d65897dff68e971cd7bc91551e913c
Brandon Weaver <keystonelemur@gmail.com>
2014-12-23 20:42:04 -0800

    Update 1_two_sum.rb

--- 1_two_sum.rb
+++ 1_two_sum.rb
@@ -1,5 +1,12 @@
 def two_sum(numbers, target)
+  # Should probably look into creating a Lazy permutation for Enumerable, this could get slow
   numbers.permutation(2).find { |n| n.reduce(:+) == target }.map { |n| ary.index(n) }
 end
 
-two_sum ARGV.shift
+describe '#two_sum' do
+  context 'When given the numbers 2, 7, 11, and 15 with a target of 9' do
+    it 'returns the indexes 0 and 1' do
+      expect(two_sum([2, 7, 11, 15], 9)).to eq([0, 1])
+    end
+  end
+end
