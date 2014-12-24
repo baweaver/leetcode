b9c774ff7d03057f2c7e702c2d6f23d0b71abfa4
Brandon Weaver <keystonelemur@gmail.com>
2014-12-23 20:43:09 -0800

    Update 1_two_sum.rb

--- 1_two_sum.rb
+++ 1_two_sum.rb
@@ -1,3 +1,14 @@
+# Given an array of integers, find two numbers such that they add up to a specific target number.
+#
+# The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+#
+# You may assume that each input would have exactly one solution.
+#
+# Input: numbers={2, 7, 11, 15}, target=9
+# Output: index1=1, index2=2
+#
+# Src: https://oj.leetcode.com/problems/two-sum/
+
 def two_sum(numbers, target)
   # Should probably look into creating a Lazy permutation for Enumerable, this could get slow
   numbers.permutation(2).find { |n| n.reduce(:+) == target }.map { |n| ary.index(n) }
